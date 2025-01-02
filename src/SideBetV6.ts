bd426bebe0112e9e82b2a5fb1f321a7fa58ae944
prakhar17072004 <pmankur2004@gmail.com>
2025-01-02 21:14:56 +0530

    new added

--- src/SideBetV6.ts
+++ src/SideBetV6.ts
@@ -1,19 +1,19 @@
-import { ponder } from "ponder:registry";
-// import { blitmapTokens } from "ponder:schema";
+// import { ponder } from "ponder:registry";
+// // import { blitmapTokens } from "ponder:schema";
  
-ponder.on("SideBetV6:SideBetEventInitialized", async ({ event, context }) => {
-  const network = context.network.name
-  const db = context.db
-  await db.insert(schema.prediction).values({
-    id: `0x${stringToHex(`${event.args.eventCode}${network}`)}`,
-    network: network,
-    predictionCode: event.args.eventCode,
-    teamA: event.args.teamA,
-    teamB: event.args.teamB,
-    standardToken: event.args.standardToken,
-    startTime: event.args.startTime,
-    endTime: event.args.endTime,
-    txid: event.transaction.hash,
-    createAt: event.block.timestamp
-  })
-});
+// ponder.on("SideBetV6:SideBetEventInitialized", async ({ event, context }) => {
+//   const network = context.network.name
+//   const db = context.db
+//   await db.insert(schema.prediction).values({
+//     id: `0x${stringToHex(`${event.args.eventCode}${network}`)}`,
+//     network: network,
+//     predictionCode: event.args.eventCode,
+//     teamA: event.args.teamA,
+//     teamB: event.args.teamB,
+//     standardToken: event.args.standardToken,
+//     startTime: event.args.startTime,
+//     endTime: event.args.endTime,
+//     txid: event.transaction.hash,
+//     createAt: event.block.timestamp
+//   })
+// });
