6ed1b0b3c136eea3053a912d8f22e94f2bbecfd2
prakhar17072004 <pmankur2004@gmail.com>
2025-01-06 18:38:24 +0530

    schema is added

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -6,7 +6,7 @@ export const NETWORK_ENUM = onchainEnum("network",["taiko"])
 export const prediction = onchainTable("prediction",(t) => ({
     id: t.hex().notNull(),
     network: NETWORK_ENUM("network").notNull(),
-    predictionCode: t.text().notNull(),
+    EventCode: t.text().notNull(),
     teamA: t.text().notNull(),
     teamB: t.text().notNull(),
     standardToken: t.hex().notNull(),
@@ -22,7 +22,8 @@ export const prediction = onchainTable("prediction",(t) => ({
 export const deposit = onchainTable("deposit", (t) => ({
     id: t.hex().$default(() => `0x${generateRandomHexString()}`),
     network: NETWORK_ENUM("network").notNull(),
-    predictionCode: t.text().notNull(),
+    EventId: t.hex().notNull(),
+    EventCode: t.text().notNull(),
     amount: t.bigint().notNull(),
     teamIndex: t.integer().notNull(),
     teamName: t.text().notNull(),
@@ -31,3 +32,28 @@ export const deposit = onchainTable("deposit", (t) => ({
   (table) => ({
     pk: primaryKey({ columns: [table.id, table.network] }),
   }))
+
+
+  export const predictionRelations = relations(prediction, ({ many, one }) => ({
+    deposits: many(deposit),
+    // rewards: many(reward),
+    // refunds: many(refund),
+    // fee: one(fee, {
+    //   fields: [prediction.id, prediction.network],
+    //   references: [fee.predictionId, fee.network],
+    // }),
+    // result: one(result, {
+    //   fields: [prediction.id, prediction.network],
+    //   references: [result.predictionId, result.network],
+    // }),
+    // cancel: one(cancel, {
+    //   fields: [prediction.id, prediction.network],
+    //   references: [cancel.predictionId, cancel.network],
+    // }),
+  }));
+  export const depositRelations = relations(deposit, ({ one }) => ({
+    prediction: one(prediction, {
+      fields: [deposit.predictionId, deposit.network],
+      references: [prediction.id, prediction.network],
+    }),
+  }));
--- src/SideBetV6.ts
+++ src/SideBetV6.ts
@@ -1,34 +1,44 @@
 import { ponder } from "ponder:registry";
-import schema, { } from "ponder:schema";
- import { generateRandomHexString, stringToHex } from "../Utils/helper"
+import schema, { Refund, Reward } from "ponder:schema";
+import { generateRandomHexString, stringToHex } from "../utils/helper";
 
 ponder.on("SideBetV6:SideBetEventInitialized", async ({ event, context }) => {
-  const network = context.network.name
-  const db = context.db
+  const network = context.network.name;
+  const db = context.db;
   await db.insert(schema.prediction).values({
     id: `0x${stringToHex(`${event.args.eventCode}${network}`)}`,
     network: network,
+    EventCode: event.args.eventCode,
     teamA: event.args.teamA,
     teamB: event.args.teamB,
     standardToken: event.args.standardToken,
     startTime: event.args.startTime,
     endTime: event.args.endTime,
-    
-  })
+  
+    createAt: event.block.timestamp,
+  });
 });
 
 ponder.on("SideBetV6:Deposited", async ({ event, context }) => {
-  const network = context.network.name
-  const db = context.db
-  const eventData = await db.find(schema.prediction, 
-    {id: `0x${stringToHex(`${event.args.eventCode}${network}`)}`, network:network})
-  if(eventData){
-      await db.insert(schema.deposit).values({
-          network: network,
-          amount: event.args.amount,
-          teamIndex: event.args.teamIndex,
-          teamName: event.args.teamIndex === 0 ? eventData.teamA : eventData.teamB,
-          
-      })
+  const network = context.network.name;
+  const db = context.db;
+  const eventData = await db.find(schema.prediction, {
+    id: `0x${stringToHex(`${event.args.eventCode}${network}`)}`,
+    network: network,
+  });
+  if (eventData) {
+    await db.insert(schema.deposit).values({
+      network: network,
+      EventId: eventData.id,
+      EventCode: event.args.eventCode,
+      amount: event.args.amount,
+      teamIndex: event.args.teamIndex,
+      teamName: event.args.teamIndex === 0 ? eventData.teamA : eventData.teamB,
+      
+      standardToken: eventData.standardToken,
+      txid: event.transaction.hash,
+      createAt: event.block.timestamp,
+    });
+    
   }
 });
