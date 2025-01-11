6708b71cf93512386c52f4e76b46928448df1139
prakhar17072004 <pmankur2004@gmail.com>
2025-01-11 22:47:23 +0530

    new added network is

--- ponder.config.ts
+++ ponder.config.ts
@@ -3,23 +3,23 @@ import { createConfig } from "ponder";
 import { http } from "viem";
 
 import { SideBetV6Abi } from "./abis/SideBetV6Abi";
-import { holesky } from "viem/chains";
 
 export default createConfig({
   networks: {
-    holesky: {
-      chainId:17000,
-      transport: http(process.env.PONDER_RPC_URL_17000),
+    taiko: {
+      chainId: 167000,
+      transport: http(process.env.PONDER_RPC_URL_167000),
     }
   },
   contracts: {
     SideBetV6: {
-      abi:SideBetV6Abi,
-      address: "0x5a2417FfC4964652DABBF24Af6dc72cA43D62c62",
+      abi: SideBetV6Abi,
+      address: "0x9ef9c57754ed079d750016b802dccd45d0ab66f8",
       network: {
-        holesky: {
-          startBlock: 3108205,
-        }
+        taiko: {
+          startBlock: 414069,
+        },
+      
       }
     },
   },
